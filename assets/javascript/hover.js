74896568d4ff52fd8e83a86f9faf287cec97540d
Robert Marks <rmarks@nav.com>
2020-06-27 16:44:38 -0600

    update modules

--- assets/javascript/hover.js
+++ assets/javascript/hover.js
@@ -1,50 +1,46 @@
-export function Hover() {
-  const target = document.querySelector('.target');
-  const links = document.querySelectorAll('.mynav a');
+const target = document.querySelector('.target');
+const links = document.querySelectorAll('.mynav a');
 
-  function mouseenterFunc() {
-    if (!this.parentNode.classList.contains('active')) {
-      for (let i = 0; i < links.length; i++) {
-        if (links[i].parentNode.classList.contains('active')) {
-          links[i].parentNode.classList.remove('active');
-        }
-        links[i].style.opacity = '0.5';
-        links[i].style.fontWeight = '200';
+function mouseenterFunc() {
+  if (!this.parentNode.classList.contains('active')) {
+    for (let i = 0; i < links.length; i++) {
+      if (links[i].parentNode.classList.contains('active')) {
+        links[i].parentNode.classList.remove('active');
       }
-
-      this.parentNode.classList.add('active');
-      this.style.opacity = '1';
-      this.style.fontWeight = '200';
-
-      const rect = this.getBoundingClientRect();
-      const { width, height, left, top } = rect;
-
-      target.style.width = `${width}px`;
-      target.style.height = `${height}px`;
-      target.style.left = `${left}px`;
-      target.style.top = `${top}px`;
-      target.style.borderColor = '#e1e2e1';
-      target.style.transform = 'none';
-      target.style.zIndex = 'auto';
+      links[i].style.opacity = '0.5';
+      links[i].style.fontWeight = '200';
     }
+
+    this.parentNode.classList.add('active');
+    this.style.opacity = '1';
+    this.style.fontWeight = '200';
+
+    const rect = this.getBoundingClientRect();
+    const { width, height, left, top } = rect;
+
+    target.style.width = `${width}px`;
+    target.style.height = `${height}px`;
+    target.style.left = `${left}px`;
+    target.style.top = `${top}px`;
+    target.style.borderColor = '#e1e2e1';
+    target.style.transform = 'none';
+    target.style.zIndex = 'auto';
   }
+}
 
-  for (let i = 0; i < links.length; i++) {
-    links[i].addEventListener('click', (e) => e.preventDefault());
-    links[i].addEventListener('mouseenter', mouseenterFunc);
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', (e) => e.preventDefault());
+  links[i].addEventListener('mouseenter', mouseenterFunc);
+}
+
+export default function resizeFunc() {
+  const active = document.querySelector('.mynav li.active');
+
+  if (active) {
+    const left = active.getBoundingClientRect().left + window.pageXOffset;
+    const top = active.getBoundingClientRect().top + window.pageYOffset;
+
+    target.style.left = `${left}px`;
+    target.style.top = `${top}px`;
   }
-
-  function resizeFunc() {
-    const active = document.querySelector('.mynav li.active');
-
-    if (active) {
-      const left = active.getBoundingClientRect().left + window.pageXOffset;
-      const top = active.getBoundingClientRect().top + window.pageYOffset;
-
-      target.style.left = `${left}px`;
-      target.style.top = `${top}px`;
-    }
-  }
-
-  window.addEventListener('resize', resizeFunc);
 }
